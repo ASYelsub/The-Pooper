ff93fcab7ab631f945c4bd7decaf6f389dbc4cb9
Kirijo <asyelsub@gmail.com>
2019-12-03 17:15:59 -0500

    Continuing to edit start scene, added James_Scene audio source with music

--- Assets/Scripts/TitleScreen.cs
+++ Assets/Scripts/TitleScreen.cs
@@ -13,6 +13,7 @@ public class TitleScreen : MonoBehaviour
     public GameObject directions;
     public Outline playOutline;
     public Outline creditsOutline;
+    public AudioSource blip;
 
     public void Start()
     {
@@ -32,11 +33,13 @@ public class TitleScreen : MonoBehaviour
         {
             playOutline.GetComponent<Outline>().enabled = true;
             creditsOutline.GetComponent<Outline>().enabled = false;
+            blip.Play();
         }
         if (outlineInt == 1)
         {
             playOutline.GetComponent<Outline>().enabled = false;
             creditsOutline.GetComponent<Outline>().enabled = true;
+            blip.Play();
         }
         if (outlineInt == 2)
         {
@@ -55,6 +58,7 @@ public class TitleScreen : MonoBehaviour
             play1Button.SetActive(true);
             thePooper.SetActive(false);
             directions.SetActive(true);
+            blip.Play();
         }
         if (buttonInt == 1)
         {
