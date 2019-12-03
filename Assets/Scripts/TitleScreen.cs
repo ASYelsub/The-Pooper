0b41c84b98d7c2564bd6ac075c423b0d6f4fe237
Kirijo <asyelsub@gmail.com>
2019-12-03 16:38:06 -0500

    Start screen UI working w/o tutorial

--- Assets/Scripts/TitleScreen.cs
+++ Assets/Scripts/TitleScreen.cs
@@ -1,18 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class TitleScreen : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject playButton;
+    public GameObject creditsButton;
+    public Outline playOutline;
+    public Outline creditsOutline;
 
-    // Update is called once per frame
-    void Update()
+    public void Start()
     {
-        
+        playOutline.GetComponent<Outline>().enabled = false;
+        creditsOutline.GetComponent<Outline>().enabled = false;
+    }
+    public void ChangeOutline(int outlineInt)
+    {
+        if(outlineInt == 0)
+        {
+            playOutline.GetComponent<Outline>().enabled = true;
+            creditsOutline.GetComponent<Outline>().enabled = false;
+        }
+        if (outlineInt == 1)
+        {
+            playOutline.GetComponent<Outline>().enabled = false;
+            creditsOutline.GetComponent<Outline>().enabled = true;
+        }
+
+    }
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(1);
     }
 }
