da7a7605e71b709013efd3883886c809660e9b86
Andrew DIVittorio <18adivitto@gmail.com>
2019-12-05 16:59:05 -0500

    trying to debug the dialogue system

--- Assets/Scripts/TextManager.cs
+++ Assets/Scripts/TextManager.cs
@@ -32,31 +32,31 @@ public class TextManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (characterURTalkingTo.GetComponentInChildren<NPCSpriteScript>().name == "ZARA")
+        if (me.characterURTalkingTo.GetComponentInChildren<NPCSpriteScript>().name == "ZARA")
         {
             UGUI.color = characterURTalkingTo.GetComponentInChildren<NPCSpriteScript>().charColors[1];
         }
-        else if (characterURTalkingTo.GetComponentInChildren<NPCSpriteScript>().name == "ISHA")
+        else if (me.characterURTalkingTo.GetComponentInChildren<NPCSpriteScript>().name == "ISHA")
         {
             UGUI.color = characterURTalkingTo.GetComponentInChildren<NPCSpriteScript>().charColors[3];
         }
-        else if (characterURTalkingTo.GetComponentInChildren<NPCSpriteScript>().name == "HELEN")
+        else if (me.characterURTalkingTo.GetComponentInChildren<NPCSpriteScript>().name == "HELEN")
         {
             UGUI.color = characterURTalkingTo.GetComponentInChildren<NPCSpriteScript>().charColors[5];
         }
-        else if (characterURTalkingTo.GetComponentInChildren<NPCSpriteScript>().name == "CANDY")
+        else if (me.characterURTalkingTo.GetComponentInChildren<NPCSpriteScript>().name == "CANDY")
         {
             UGUI.color = characterURTalkingTo.GetComponentInChildren<NPCSpriteScript>().charColors[6];
         }
-        else if (characterURTalkingTo.GetComponentInChildren<NPCSpriteScript>().name == "CARLA")
+        else if (me.characterURTalkingTo.GetComponentInChildren<NPCSpriteScript>().name == "CARLA")
         {
             UGUI.color = characterURTalkingTo.GetComponentInChildren<NPCSpriteScript>().charColors[4];
         }
-        else if (characterURTalkingTo.GetComponentInChildren<NPCSpriteScript>().name == "QUAFT")
+        else if (me.characterURTalkingTo.GetComponentInChildren<NPCSpriteScript>().name == "QUAFT")
         {
             UGUI.color = characterURTalkingTo.GetComponentInChildren<NPCSpriteScript>().charColors[1];
         }
-        else if (characterURTalkingTo.GetComponentInChildren<NPCSpriteScript>().name == "ELIKENE")
+        else if (me.characterURTalkingTo.GetComponentInChildren<NPCSpriteScript>().name == "ELIKENE")
         {
             UGUI.color = characterURTalkingTo.GetComponentInChildren<NPCSpriteScript>().charColors[1];
         }
